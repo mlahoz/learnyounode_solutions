6c073f5037eafdab57b240af59ad589e221fd8ed
Miguel Lahoz <miguel.lahoz@gmail.com>
2015-11-05 08:32:38 +0100

    My first async I/O

--- program.js
+++ program.js
@@ -3,6 +3,9 @@ var fs = require('fs');
 if (process.argv.length > 2) {
 
     var filename = process.argv[2];
-    var lines = fs.readFileSync(filename).toString().split('\n');
-    console.log(lines.length - 1);
+    fs.readFile(filename, 'utf8', function (error, data) {
+	if (!error) {
+	    console.log(data.split('\n').length - 1);
+	}
+    });
 }
