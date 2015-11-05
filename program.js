ec8fc4bb6fa09fab08bd6f535f50dfe77693ef93
Miguel Lahoz <miguel.lahoz@gmail.com>
2015-11-05 08:45:41 +0100

    Time server

--- program.js
+++ program.js
@@ -1,65 +1,16 @@
-var http = require('http');
-var concat = require('concat-stream');
+var net = require('net');
+var strftime = require('strftime');
 
-if (process.argv.length === 5) {
+var port = 8888;
 
-    var url1 = process.argv[2];
-    var url2 = process.argv[3];
-    var url3 = process.argv[4];
-
-    var urls = [];
-
-    var addUrl = function (url) {
-
-	urls.push({
-	    url: url,
-	    data: '',
-	    done: false
-	});
-
-    };
-
-    addUrl(url1);
-    addUrl(url2);
-    addUrl(url3);
-
-    var isDone = function () {
-	return urls.reduce(function (acc, next) {
-	    return acc && next.done;
-	}, true);
-    };
-
-    var setDone = function (index) {
-	urls[index].done = true;
-	return isDone();
-    };
-
-    var setData = function (index, data) {
-	urls[index].data = data;
-	return setDone(index);
-    };
-
-    var printData = function () {
-
-	urls.forEach(function (item) {
-	    console.log(item.data);
-	});
-
-    };
-
-    var requestData = function (index) {
-
-	http.get(urls[index].url, function (response) {
-	    response.pipe(concat(function (data) {
-		if (setData(index, data.toString())) {
-		    printData();
-		}
-	    }));
-	});
-    };
-
-    for (var i = 0; i < urls.length; i++) {
-	requestData(i);
-    }
-
+if (process.argv.length > 2) {
+    port = process.argv[2];
 }
+
+var server = net.createServer(function (socket) {
+
+    socket.write(strftime('%Y-%m-%d %H:%M'));
+
+    socket.end('\n');
+});
+server.listen(port);
